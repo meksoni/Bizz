1b0bfe5bbfd00c08d9df5927da1f86c1673ba7bd
meksoni <mdesign.rss@gmail.com>
2024-09-18 12:14:33 +0200

    create brand & category resources

---
+++ app/Models/Product.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'category_id',
+        'brand_id',
+        'name',
+        'slug',
+        'images',
+        'description',
+        'price',
+        'is_active',
+        'is_featured',
+        'in_stock',
+        'on_sale',
+    ];
+
+    protected $casts = [
+        'images' => 'array',
+    ];
+
+    public function category(): BelongsTo {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function brand(): BelongsTo {
+        return $this->belongsTo(Brand::class);
+    }
+
+    public function orderItem(): HasMany {
+        return $this->hasMany(OrderItem::class);
+    }
+}
